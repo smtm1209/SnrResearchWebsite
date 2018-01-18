e920e46a5d4e284223bb478dc77c6410165e7d15
Sarkis Ter Martirosyan <sarkistm@hotmail.com>
2018-01-17 21:25:12 -0500

    Fix pictures

--- js/index.js
+++ js/index.js
@@ -7,4 +7,12 @@ $(document).ready(function() {
 			$(this).children('#nav_dropdown').slideUp(200);
 		}
 	);
+	$('#nav_paper').hover(
+		function() {
+			$(this).children('#paper_dropdown').slideDown(200);
+		}, 
+		function() {
+			$(this).children('#paper_dropdown').slideUp(200);
+		}
+	);
 });
